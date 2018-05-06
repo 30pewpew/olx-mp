802d734c9fe4c955ecfb75016426c7689d9cec3f
Esy <30pewpew@gmail.com>
2018-05-06 13:50:42 +0800

    Readjust commonbar.php
    * Adjusted images
    * Clickable image

--- commonbar.php
+++ commonbar.php
@@ -15,11 +15,12 @@
 			</head>
 		    
 		    <body>
-				<div class="container-fluid" style="background-image: url(images/background_by_hadouuuken-dc9yxs6.png);">  
-					<img src = "images/imageedit_7_5986917331_by_hadouuuken-dc9yxd0.png" align = "middle" style = "width: 100% height: 100%;"></img>
-			        
-					<h4 style = "text-align:center"><span class="label label-default">BUY</span>, <span class="label label-default">SELL</span>, <span class="label label-default">LEND</span>, <span class="label label-default">BORROW</span></h4></div>
-		    	</div>
+				<div class="container-fluid" style="background-image: url(images/background_by_hadouuuken-dc9yxs6.png);">
+		        <a href="index.php">   
+				<img src = "images/imageedit_7_5986917331_by_hadouuuken-dc9yxd0.png" align = "middle" style = "margin-top: 2%; margin-left: 2%; width: 250px; height: 115px;"></img>
+		        </a>
+				<h4 style="text-align:center"><span class="label label-default">BUY</span> <span class="label label-default">SELL</span> <span class="label label-default">LEND</span> <span class="label label-default">BORROW</span></h4>
+		    </div>
 
 <?php
 	}
