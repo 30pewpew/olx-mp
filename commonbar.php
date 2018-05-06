4a07cb0a451f39ea7ad6e9ea4d4767cc72487594
Esy <30pewpew@gmail.com>
2018-05-06 13:25:36 +0800

    Code reformatting
    * Reformatted index.php
    * Testing out commonbar.php

--- commonbar.php
+++ commonbar.php
@@ -1,25 +1,26 @@
 <?php
-function showheader($title){
-	?>
+	function showheader($title)
+	{
+?>
     
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title><?php echo $title; ?></title>
-	<link rel="stylesheet" href="style/css/index.css">
-	<link rel="stylesheet" href="style/css/bootstrap.min.css">
-    <script src="style/js/jquery.min.js"></script>
-    <script src="style/js/bootstrap.min.js"></script>
-</head>
-    <body>
-	<div class="container-fluid" style="background-image: url(images/background_by_hadouuuken-dc9yxs6.png);">
-            
-		<img src = "images/imageedit_7_5986917331_by_hadouuuken-dc9yxd0.png" align = "middle" style = "width: 100px; height: 50px;"></img>
-        
-	<h4 style="text-align:center"><span class="label label-default">buy</span> or <span class="label label-default">sell</span>, <span class="label label-default">lend</span> or <span class="label label-default">borrow</span></h4>
-    <hr>
-    </div>
+		<!DOCTYPE HTML>
+			<html>
+			<head>
+				<meta charset="utf-8">
+				<title><?php echo $title; ?></title>
+				<link rel="stylesheet" href="style/css/index.css">
+				<link rel="stylesheet" href="style/css/bootstrap.min.css">
+			    <script src="style/js/jquery.min.js"></script>
+			    <script src="style/js/bootstrap.min.js"></script>
+			</head>
+		    
+		    <body>
+				<div class="container-fluid" style="background-image: url(images/background_by_hadouuuken-dc9yxs6.png);">  
+					<img src = "images/imageedit_7_5986917331_by_hadouuuken-dc9yxd0.png" align = "middle" style = "width: 100% height: 100%;"></img>
+			        
+					<h4 style = "text-align:center"><span class="label label-default">BUY</span>, <span class="label label-default">SELL</span>, <span class="label label-default">LEND</span>, <span class="label label-default">BORROW</span></h4></div>
+		    	</div>
+
 <?php
-}
+	}
 ?>
